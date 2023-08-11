d555ebb410903e38c8bb304555ae3a847fb2dd20
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-11 11:33:02 -0700

    added 9-print_comb file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		if (ch != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
