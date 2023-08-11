23b66634d7ea4a38f4e2b8f3fa6685622aec7e3b
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-11 10:30:52 -0700

    update 4-print_alphabet file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,13 +11,13 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if(ch != 'q' && ch != 'e')
+		if (ch != 'q' && ch != 'e')
 		{
 			putchar(ch);
 		}
 		ch++;
 	}
-       
+
 	putchar('\n');
 
        	return (0);
