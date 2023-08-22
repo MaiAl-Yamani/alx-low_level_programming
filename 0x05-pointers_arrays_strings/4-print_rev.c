3b349b08eb413c5dc1bf20634b2d38dbf9b38b9c
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-21 18:26:20 -0700

    added 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: pointer to s
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	for (; *s == '\0'; s--)
+	{
+		_putchar(*s);
+	}
+
+	_putchar('\n');
+}
