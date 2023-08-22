f98dda46ba8f92fe4e646d7dfe6103badc29e044
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-21 17:49:43 -0700

    added 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen - return the length of a string
+ * @s: pinter to char s
+ *
+ * Description: returns the length of a string
+ * return: int i 
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	for (; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
