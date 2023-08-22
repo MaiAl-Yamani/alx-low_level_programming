c9b06e6fda8a6b78fabe23aa97a0a752a33fa3e7
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-21 19:44:09 -0700

    added 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of the string
+ * @str: pointer to char str
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n;
+
+	int ni = (strlen(str) - 1) / 2;
+	for (n = ni; n < strlen(str); n++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
