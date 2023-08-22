324c99a61b2947bb3bc242fed77f209136378cf1
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-21 19:58:01 -0700

    added 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to a
+ * @n: number of elements in the array to be printed
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+
+}
