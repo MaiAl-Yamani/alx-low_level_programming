9e0e15c53fee65499ce92216d4a161918aaf115a
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-21 19:49:28 -0700

    added 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,9 @@ void puts_half(char *str)
 	int n;
 
 	int ni = (strlen(str) - 1) / 2;
-	for (n = ni; *str != '\0'; n++)
+	for (n = ni; *str != '\0'; str++)
 	{
-		_putchar(*str);
+		_putchar(str[n]);
 	}
 
 	_putchar('\n');
