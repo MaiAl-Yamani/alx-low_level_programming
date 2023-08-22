bdf552c644a06b196aa90a3d4c0954246a067e23
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-21 19:03:34 -0700

    added 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer to s
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+        int n;
+
+        for (n = -2; ; s--)
+        {
+                _putchar(s[n]);
+                n--;
+        }
+
+        _putchar('\n');
+}
