dcb4c5fba38c775147a4e4920338886911c8760a
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-24 16:45:38 -0700

    added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to 1st string
+ * @s2: pointer to 2nd string
+ *
+ * Return: 0 if s1=s2, -ve if s1<s2, +ve if s1>s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s1_len = strlen(s1);
+	int s2_len = strlen(s2);
+
+	if (s1_len == s2_len)
+	{
+		return (0);
+	}
+	else if (s1_len < s2_len)
+	{
+		return (-15);
+	}
+	else
+	{
+		return (+15);
+	}
+	return (0);
+}
