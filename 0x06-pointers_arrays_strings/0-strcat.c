ab7e32ec89042fae38f882cd8d2d29c210ac16f0
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-24 15:55:11 -0700

    eddited betty in 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 #include <string.h>
 /**
  * _strcat - concatinate two string variables
- * @src: the first string variable (pointer to src)
- * @dest: the second string variable (pointer to dest)
+ * @src: the first string variable pointer to src
+ * @dest: the second string variable pointer to dest
  *
  * Description: concatinate two string variables and returns the concatinated string
  * Return: dest variable after concatination
