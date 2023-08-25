56f8037549225b370432e8ce0611f5227c13aa1f
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-25 16:18:44 -0700

    added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "min.h"
+/**
+ * _islower - checks for lowercase characters
+ * @c: charcter input to check if it is lowercase or not
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	int res;
+
+	if (c <= 97 && c <= 122)
+	{
+		res = 1;
+	}
+	else
+	{
+		res = 0;
+	}
+
+	return (res);
+}
