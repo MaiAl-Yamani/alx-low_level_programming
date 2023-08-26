201bd0b00c727bd58306546ac81d794b996cd247
Mai Elyamani <mai.elyamni@gmail.com>
2023-08-25 17:16:40 -0700

    added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every min of the day starting from 00:00 to 23:59.
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	/* ij:kl */
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(j);
+			_putchar(':');
+			for (k = 0; k < 10; k++)
+			{
+				_putchar(k);
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
