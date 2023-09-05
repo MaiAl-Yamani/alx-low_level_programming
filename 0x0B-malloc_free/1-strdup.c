44f7f70264a802960100d5a0216657b9552f3d14
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-05 05:50:59 -0700

    added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns newstr pointer which is a duplicate of str
+ * @str: pointer to string
+ *
+ * Return: a pointer to a new string
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+	unsigned int i;
+
+	newstr = malloc(sizeof(str));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < sizeof(str))
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	return (newstr);
+}
