b14d6792f9eebd3cbfca52ec3bbaeb66c6f5e922
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-05 09:32:07 -0700

    updated 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,13 +20,13 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < ac; i++)
+	while (i < ac)
 	{
 		while (av[i][j] != 0)
 		{
 			str_l++, j++;
 		}
-		str_l++;
+		str_l++, i++;
 	}
 	str_l++;
 	concat_res = malloc(str_l * sizeof(char));
