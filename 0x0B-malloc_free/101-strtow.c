92b58cd56cd273e46a2d4259705d6cf740149a71
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-05 15:27:25 -0700

    added 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * word_len - returns the length of the initial word in the string
+ * @str: input string
+ *
+ * Return: length of first word
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && (*(str + index) != ' '))
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
+/**
+ * words_count - returns the count of words in a string
+ * @str: input string
+ *
+ * Return: count of words
+ */
+int words_count(char *str)
+{
+	int index = 0, ar_len = 0, word_count = 0;
+
+	for (index = 0; *(str + index); index++)
+		ar_len++;
+	for (index = 0; index < ar_len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			word_count++;
+			index += word_len(str + index);
+		}
+	}
+	return (word_count);
+}
+/**
  * strtow - splits a string into words.
  * @str: input string to be split
  *
@@ -10,26 +49,39 @@
 char **strtow(char *str)
 {
 	char **f_ptr;
-	int j, k;
-	unsigned int i;
+	int word_count, w, letter_count, l, index = 0;
 
-	if (str == NULL || str == 0)
+	if (str == NULL || str[0] == 0)
 	{
 		return (NULL);
 	}
-	f_ptr = (char **)malloc(2 * strlen(str));
+	word_count = words_count(str);
+	if (word_count == 0)
+		return (NULL);
+	f_ptr = (char **)malloc(sizeof(char *) * (word_count + 1));
 	if (f_ptr == NULL)
 	{
-		free(f_ptr);
-		return(NULL);
+		return (NULL);
 	}
-	k = 0;
-	j = 0;
-	for (i = 0; i < strlen(str) ; i++)
+	for (w = 0; w < word_count; w++)
 	{
-		f_ptr[i][j] = str[k];
-		k++;
+		while (str[index] == ' ')
+			index++;
+
+		letter_count = word_len(str + index);
+		f_ptr[w] = malloc(sizeof(char) * (letter_count + 1));
+
+		if (f_ptr[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(f_ptr[w]);
+			free(f_ptr);
+			return (NULL);
+		}
+		for (l = 0; l < letter_count; l++)
+			f_ptr[w][l] = str[index++];
+		f_ptr[w][l] = '\0';
 	}
-	f_ptr[i][j] = '\0';
+	f_ptr[w] = NULL;
 	return (f_ptr);
 }
