5767232f43e4b21dbd473052b6afed9aebcb2165
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-05 09:54:53 -0700

    modified 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * strtow - splits a string into words.
  * @str: input string to be split
@@ -8,3 +9,28 @@
  */
 char **strtow(char *str)
 {
+	char **f_ptr;
+	int i, j, k;
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+	f_ptr = (char **)malloc();
+	if (f_ptr == NULL)
+	{
+		free(f_ptr);
+		return(NULL);
+	}
+	k = 0;
+	for (i = 0; i < ; i++)
+	{
+		for (j = 0; j < strlen(str[k]); j++)
+		{
+			f_ptr[i][j] = str[k];
+		}
+		f_ptr[i][j] = '\0';
+	}
+	f_ptr[i][j] = '\0';
+	return (f_ptr);
+}
