4ebcb5f9217d612ec3d7d9984b087f64fbb29a5e
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-08 17:58:37 -0700

    added 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,9 +17,12 @@ int *array_range(int min, int max)
 	ptr = malloc(sizeof(int) * ((max - min) + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = min; i < (max + 1); i++)
+	i = 0;
+	while (min <= max)
 	{
-		ptr[i] = i;
+		ptr[i] = min;
+		min++;
+		i++;
 	}
 	return (ptr);
 }
