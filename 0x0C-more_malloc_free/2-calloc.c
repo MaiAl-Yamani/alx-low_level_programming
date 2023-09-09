e74ca418bec1760d3ee4b6a09acde4452ce6c06a
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-08 17:43:37 -0700

    added 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	calloc = malloc(nmemb * size);
 	if (calloc == NULL)
 		return (NULL);
-	for (i = 0 ; i < nmemb; i++)
+	for (i = 0 ; i < (nmemb * size); i++)
 	{
 		calloc[i] = 0;
 	}
