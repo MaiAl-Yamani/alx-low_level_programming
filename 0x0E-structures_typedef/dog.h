22d8eec0b068284c27b250a0e4f7ea80583312df
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-11 02:22:02 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: structure foe dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
