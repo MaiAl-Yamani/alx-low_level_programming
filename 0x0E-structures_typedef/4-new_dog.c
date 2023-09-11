f6f9739c6750a7c08f484c889f33469b24d26449
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-11 03:49:58 -0700

    update in 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,14 +26,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	cpname = malloc(len_name + 1);
+	cpname = malloc(len_name);
 	if (cpname == NULL)
 		return (NULL);
 	for (i = 0; name[i]; i++)
 		cpname[i] = name[i];
 	cpname[i] = '\0';
 
-	cpowner = malloc(len_owner + 1);
+	cpowner = malloc(len_owner);
 	if (cpowner == NULL)
 		return (NULL);
 	for (i = 0; owner[i]; i++)
