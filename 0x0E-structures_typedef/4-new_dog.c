be650c0bf84121ea00e4aea89327e0889837a122
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-11 18:42:42 -0700

    updated 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,6 +34,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < len_name; i++)
 		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
 
 	new_dog->age = age;
 
@@ -45,5 +46,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < len_owner; i++)
 		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
 	return (new_dog);
 }
