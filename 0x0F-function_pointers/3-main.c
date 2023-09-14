424792d709710695655526b2597093d089cee6aa
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-13 20:41:51 -0700

    updated 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,7 @@
  *
  * Return: always 0 (success)
  */
-int main(int argc, int *argv)
+int main(int argc, char *argv[])
 {
 	int a, b;
 	int (*o)(int, int);
@@ -18,17 +18,18 @@ int main(int argc, int *argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	o = get_op_func(argv[2]);
 	if (o == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (argv[3] == 0 && (argc[2] == '/' || argv[2] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	printf("%d\n", o(a, b));
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -10,11 +10,10 @@
  */
 int op_add(int a, int b)
 {
-	op_t *add_ptr;
-
-	add_ptr->op = '+';
-	add_ptr->(*f)(int, int) = a + b;
-	return (add_ptr->(*f)(int, int));
+	/* op_t *add_ptr;*/
+	/*add_ptr->op = "+";*/
+	/*add_ptr->f = (*get_op_func)(a, b); */
+	return(a + b);
 }
 /**
  * op_sub - subtract two integers
@@ -25,11 +24,10 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	op_t *sub_ptr;
-
-	sub_ptr->op  = '-';
-	sub_ptr->(*f)(int, int) = a - b;
-	return (sub_ptr->(*f)(int, int));
+	/*op_t *sub_ptr;*/
+	/*sub_ptr->op  = "-";*/
+	/*sub_ptr->(*f)(int, int) = a - b;*/
+	return (a - b);
 }
 /**
  * op_mul - multiplies two integers
@@ -40,11 +38,10 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	op_t *mul_ptr;
-
-	mul_ptr->op = '*';
-	mul_ptr->(*f)(int, int) = a * b;
-	return (mul_ptr->(*f)(int, int));
+	/*op_t *mul_ptr;*/
+	/*mul_ptr->op = "*";*/
+	/*mul_ptr->(*f)(int, int) = a * b;*/
+	return (a * b);
 }
 /**
  * op_div - divides two integers
@@ -55,16 +52,15 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	op_t div_ptr;
-
+	/*op_t div_ptr;*/
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	div_ptr->op = '/';
-	div_ptr->(*f)(int, int) = a / b;
-	return (div_ptr->(*f)(int, int));
+	/*div_ptr->op = "/";*/
+	/*div_ptr->(*f)(int, int) = a / b;*/
+	return (a / b);
 }
 /**
  * op_mod - returns remainder of division of two integers
@@ -75,14 +71,13 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	op_t mod_ptr;
-
+	/*op_t mod_ptr;*/
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	mod_ptr->op = '%';
-	mod_ptr->(*f)(int, int) = a % b;
-	return (mod_ptr->(*f)(int, int));
+	/*mod_ptr->op = "%";*/
+	/*mod_ptr->(*f)(int, int) = a % b;*/
+	return (a % b);
 }
