37c670507772cb9cd83bc4672825926e0d7a24b5
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-21 15:12:20 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,18 +12,12 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *current = *head;
-	int c;
-	char *dup_str;
 
-	dup_str = strdup(str);
 	current = *head;
 	while (current && current->next != NULL)
 	{
 		current = current->next;
 	}
-
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -37,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = c;
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 	if (current)
 		current->next = new_node;
