1a1f5df348ac60ded4a0b0d32f0cb1ffbd649a56
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-21 12:54:10 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
