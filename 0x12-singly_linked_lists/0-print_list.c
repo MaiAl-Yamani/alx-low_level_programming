522d1e5ba6528fbe7f9be3b840e62f2385d6442a
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-22 15:18:24 -0700

    some clean up

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,27 +10,20 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	int null_len = 0;
-	/*const list_t *temp;*/
 
-	/*temp = malloc(sizeof(list_t));*/
-	/*if (temp == NULL)*/
-		/*return (-1);*/
 	while (h != NULL)
 	{
 		count++;
 		if (h->str == NULL)
 		{
-			printf("[%i] (nil)\n", null_len);
+			printf("[%i] (nil)\n", 0);
 		}
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
-		/*temp = h;*/
 		h = h->next;
 	}
-	/*free(temp);*/
 	return (count);
 }
 
