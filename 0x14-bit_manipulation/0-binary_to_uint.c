f425da1962fde11857997f9fdf15ddee4d446b87
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-28 15:23:23 -0700

    added 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -29,9 +29,11 @@ unsigned int binary_to_uint(const char *b)
 	int len = strlen(b);
 	int i;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; i < len; i++)
 	{
-		if ((b[i] != '0' && b[i] != '1') || b == NULL)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		n += (b[i] - '0') * Pow(2, (len - 1 - i));
 	}
