bab794e9c185b1e26945aeeb9d66d7b546a52049
Mai Elyamani <mai.elyamni@gmail.com>
2023-09-28 15:15:16 -0700

    added 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+/**
+ * Pow - returns result of a to the power of b
+ * @a: base
+ * @b: exponent
+ *
+ * Return: power result
+ */
+int Pow(int a, int b)
+{
+	int power = 1, i;
+
+	for (i = 0; i < b; i++)
+	{
+		power = power * a;
+	}
+	return (power);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to to a string of 0 and 1 chars (binary)
+ *
+ * Return: converted number, or 0 if (chars is not 0 or 1 or b is NULL)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int len = strlen(b);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if ((b[i] != '0' && b[i] != '1') || b == NULL)
+			return (0);
+		n += (b[i] - '0') * Pow(2, (len - 1 - i));
+	}
+	return (n);
+}
