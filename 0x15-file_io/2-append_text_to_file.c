450224d3732780a4b5a6c3995de86418a375a87c
Mai Elyamani <mai.elyamni@gmail.com>
2023-10-06 13:00:22 -0700

    edited 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int f;
-	ssize_t content_len, wr_len;
+	ssize_t content_len = 0, wr_len = 0;
 	char *ptr;
 
 	if (filename == NULL)
