eac600ae6ba683ce40df933a44f03e31e62808e7
Mai Elyamani <mai.elyamni@gmail.com>
2023-10-22 21:34:27 -0700

    added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints an integer
+ * @n: int input
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int n_1;
+
+	if (n < 0)
+	{
+		n_1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n_1 = n;
+	}
+	if (n_1 / 10)
+	{
+		print_number(n_1 / 10);
+	}
+	_putchar((n_1 % 10) + '0');
+}
