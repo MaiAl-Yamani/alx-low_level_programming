449cafd410dbb5f82c0c255acac14e2bb83faae2
Mai Elyamani <mai.elyamni@gmail.com>
2023-10-31 16:04:31 -0700

    modified 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,20 @@
 
 int main(void)
 {
-	int n = 0;
-	int i = 0;
+	int d1, d2;
 
-	while (n <= 9)
+	for (d1 = 0; d1 < 10; d1++)
 	{
-		while (i <= 9)
+		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
-			putchar('0' + n);
-			putchar('0' + i);
+			putchar('0' + d1);
+			putchar('0' + d2);
+			if (d1 == 8 && d2 == 9)
+				break;
 			putchar(',');
 			putchar(' ');
-			i++;
 		}
-		n++;
 	}
 	putchar('\n');
-
 	return (0);
 }
