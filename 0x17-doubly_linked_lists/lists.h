06a6fa948a48d07da722506cc0533e65459a6211
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-21 01:47:08 +0200

    add 0-print_dlistint.c lists.h README.md

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: node's data
+ * @next: pointer to the next node
+ * @prev: pointer to previous node
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *next;
+	struct dlistint_s *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+#endif /* LISTS_H */
