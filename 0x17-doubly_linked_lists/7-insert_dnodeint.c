a3939dd033de5e9d90c93b32d46871d32de85aa2
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-21 03:59:25 +0200

    add 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = *h;
+		*h = new;
+		return (new);
+	}
 	while (current != NULL)
 	{
 		if (c == idx - 1)
