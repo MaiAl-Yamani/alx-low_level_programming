e6a33cd610ad3d87c7178100c68d746f3e4df3ca
Mai Elyamani <mai.elyamni@gmail.com>
2024-02-09 23:37:11 +0200

    add 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """A function that gets an island perimeter."""
 
+
 def island_perimeter(grid):
     """
     Returns the perimeter of an island described in grid form:
         grid is a list of list of integers:
-            0 represents a water zone
-            1 represents a land zone
-            One cell is a square with side length 1
-            Grid cells are connected horizontally/vertically (not diagonally).
-            Grid is rectangular, width and height don’t exceed 100
-            Grid is completely surrounded by water, and there is one island (or nothing).
-            The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+            #0 represents a water zone
+            #1 represents a land zone
+            #One cell is a square with side length 1
+            #Grid cells are connected horizontally/vertically (not diagonally).
+            #Grid is rectangular, width and height don’t exceed 100
+            #Grid is completely surrounded by water, and there is
+                one island (or nothing).
+            #The island doesn’t have “lakes” (water inside that isn’t
+                connected to the water around the island).
     Args:
         2D array of 0s and 1s.
     Return:
