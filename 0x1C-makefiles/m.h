589e378b61d51f1abf7164865e447eda790c93b9
Mai Elyamani <mai.elyamni@gmail.com>
2024-05-29 11:42:23 +0300

    add 0x1C-makefiles/

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,8 @@
+#ifndef _M_H_
+#define _M_H_
+
+#include <stdio.h>
+
+void print_school(void);
+
+#endif /* _M_H_ */
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "m.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main()
+{
+	print_school();
+	return 0;
+}
+/* School */
